21ace4b998124ef9be6fc0958485d738532adf45
n.shelemina <n.shelemina@dextechnology.com>
2022-04-25 08:57:34 +0300

    Конфигурация сетапа.

--- src/test/java/ru/spsu/iti/TestBase.java
+++ src/test/java/ru/spsu/iti/TestBase.java
@@ -8,6 +8,9 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.spsu.iti.helpers.AllureAttachments;
+
 
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
@@ -15,18 +18,21 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "http://iti.spsu.ru";
+        Configuration.startMaximized = true;
+        Configuration.remote = System.getProperty("url");
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("version");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     public void addAttachments() {
-//        String sessionId = DriverUtils.getSessionId();
-//        AllureAttachments.addScreenshotAs("Last screenshot");
-//        AllureAttachments.addPageSource();
-//        AllureAttachments.addBrowserConsoleLogs();
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+        AllureAttachments.addBrowserConsoleLogs();
         Selenide.closeWebDriver();
-//
-//        if (Project.isVideoOn()) {
-//            AllureAttachments.addVideo(sessionId);
-//        }
     }
 }
---
+++ src/test/java/ru/spsu/iti/helpers/AllureAttachments.java
@@ -0,0 +1,26 @@
+package ru.spsu.iti.helpers;
+
+import io.qameta.allure.Attachment;
+
+public class AllureAttachments {
+
+
+    @Attachment(value = "{attachName}", type = "text/plain")
+    private static String addMessage(String attachName, String text) {
+        return text;
+    }
+
+    public static void addBrowserConsoleLogs() {
+        addMessage("Browser console logs", DriverUtils.getConsoleLogs());
+    }
+
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] addScreenshotAs(String attachName) {
+        return DriverUtils.getScreenshotAsBytes();
+    }
+
+    @Attachment(value = "Page source", type = "text/html")
+    public static byte[] addPageSource() {
+        return DriverUtils.getPageSourceAsBytes();
+    }
+}
